c382869bcf8bdcc38ce9a71fe8a512d6e8d0a5b6
Sveta <Sveta@KOMP>
2021-12-06 12:38:05 +0300

    Добавьте файлы проекта.

---
+++ 16/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+
+
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] table1 = Print1();
+            string path2 = @"D:\Sveta\Задания\16\16\bin\Debug\Новая папка\Products.json";
+            StreamWriter sw = new StreamWriter(path2, false);
+            for (int i = 0; i < 5; i++)
+            {
+                Product product = new Product()
+                {
+                    Code_product = uint.Parse(table1[i, 0]),
+                    Name_product = table1[i, 1],
+                    Price_product = float.Parse(table1[i, 2]),
+                };
+                JsonSerializerOptions options = new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic)
+                };
+                string json = JsonSerializer.Serialize(product, options);
+                Console.WriteLine(json);
+                sw.WriteLine(json);
+            }
+            sw.Close();
+            Console.ReadKey();
+        }
+        static string[,] Print1()
+        {
+            string[,] table1 = new string[5, 3];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Введите код {0} товара: ", i + 1);
+                table1[i, 0] = Console.ReadLine();
+                Console.Write("Введите название {0} товара: ", i + 1);
+                table1[i, 1] = Console.ReadLine();
+                Console.Write("Введите цену {0} товара: ", i + 1);
+                table1[i, 2] = Console.ReadLine();
+                Console.WriteLine("\n");
+            }
+            return table1;
+        }
+    }
+    public class Product
+    {
+        public uint Code_product { get; set; }
+        public string Name_product { get; set; }
+        public float Price_product { get; set; }
+    }
+}
+
---
+++ 16_/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+
+namespace _16_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path2 = @"D:\Sveta\Задания\16\16\bin\Debug\Новая папка\Products.json";
+            Product product = new Product();
+            int str = 0;
+            int max = 0;
+            string name = null;
+            foreach (var line in File.ReadLines(path2))
+            {
+                product = JsonSerializer.Deserialize<Product>(line);               
+                int price = (int)product.Price_product;
+                Console.WriteLine(product.Code_product);
+                Console.WriteLine(product.Name_product);
+                Console.WriteLine(product.Price_product);
+                Console.WriteLine();
+                if (price > max)
+                {
+                    max = price;
+                    name = product.Name_product;
+                    str++;
+                }               
+            }           
+            Console.WriteLine("\n{0} дороже всего - цена {1} ", name, max);
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public uint Code_product { get; set; }
+        public string Name_product { get; set; }
+        public float Price_product { get; set; }
+    }
+}
